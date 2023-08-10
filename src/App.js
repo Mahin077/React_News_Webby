380dcd6f1f78d52b8d3763df43ce09df4980ae3a
Mahin077 <mmahinm10@gmail.com>
2023-08-10 21:02:38 +0600

    categories added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
 import News from './Components/News';
-
+import React,{useState} from 'react';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar'
 function App() {
+  const pageSize = 9;
+  const apiKey ="d093053d72bc40248998159804e0e67d";
+  const [loadingProgress, setLoadingProgress] = useState(0)
   return (
     <>
+    <Router>
     <Navbar/>
-    <News country="us" category="science" pageSize={6}/>
+    <Routes>
+          <Route exact path="/" element={<News setLoadingProgress={setLoadingProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general"/>}></Route> 
+          <Route exact path="/business" element={<News setLoadingProgress={setLoadingProgress} apiKey={apiKey} key="business" pageSize={pageSize} country="in" category="business"/>}></Route> 
+          <Route exact path="/entertainment" element={<News setLoadingProgress={setLoadingProgress} apiKey={apiKey} key="entertainment" pageSize={pageSize} country="in" category="entertainment"/>}></Route> 
+          <Route exact path="/general" element={<News setLoadingProgress={setLoadingProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general"/>}></Route> 
+          <Route exact path="/health" element={<News setLoadingProgress={setLoadingProgress} apiKey={apiKey} key="health" pageSize={pageSize} country="in" category="health"/>}></Route> 
+          <Route exact path="/science" element={<News setLoadingProgress={setLoadingProgress} apiKey={apiKey} key="science" pageSize={pageSize} country="in" category="science"/>}></Route> 
+          <Route exact path="/sports" element={<News setLoadingProgress={setLoadingProgress} apiKey={apiKey} key="sports" pageSize={pageSize} country="in" category="sports"/>}></Route> 
+          <Route exact path="/technology" element={<News setLoadingProgress={setLoadingProgress} apiKey={apiKey} key="technology" pageSize={pageSize} country="in" category="technology"/>}></Route> 
+        </Routes>
+    </Router>
     </>
   );
 }
--- src/Components/News.js
+++ src/Components/News.js
@@ -1,32 +1,62 @@
 import React, { useState, useEffect } from 'react'
 import NewsItem from './NewsItem'
+import PropTypes from 'prop-types'
+import Spinner from './Spinner'
+
 function News(props) {
+
     const [articles, setArticles] = useState([])
-    const [page,setPage] = useState(1)
-    const updateNews = async ()=>{
-        const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=d093053d72bc40248998159804e0e67d&page=${page}&pageSize=${props.pageSize}`;
+    const [page, setPage] = useState(1)
+    const [loading, setLoading] = useState(true)
+    const [totalResults, setTotalResults] = useState(0)
+
+    const updateNews = async () => {
+        props.setLoadingProgress(10);
+        const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+        setLoading(true)
         let data = await fetch(url);
-        let parsedData = await data.json();
-        setArticles(parsedData.articles);
-        console.log(articles)
+        props.setLoadingProgress(40);
+        let parsedData = await data.json()
+        props.setLoadingProgress(80);
+        setArticles(parsedData.articles)
+        setTotalResults(parsedData.totalResults)
+        setLoading(false)
+        props.setLoadingProgress(100);
     }
 
-    useEffect(()=>{
+    useEffect(() => {
+        document.title = `${props.category.charAt(0).toUpperCase() + props.category.slice(1)} - News Webby`;
         updateNews();
-    },[])
+    }, [])
     return (
-        <div className="container">
-            this is news
-            <div className="row">
-                {articles.map((element) => {
-                    return <div className="col-md-4" key={element.url}>
-                        <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
-                    </div>
-                })}
-            </div>
+        <>
+            <h1 className="text-center" style={{ margin: '35px 0px', marginTop: '90px' }}>News Webby - Top {props.category.charAt(0).toUpperCase() + props.category.slice(1)} Headlines</h1>
+            {loading && <Spinner/>}
+            <div className="container">
 
-        </div>
+                <div className="row">
+                    {articles.map((element) => {
+                        return <div className="col-md-4" key={element.url}>
+                            <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+                        </div>
+                    })}
+                </div>
+
+            </div>
+        </>
     )
 }
 
 export default News
+
+News.defaultProps = {
+    country: 'us',
+    pageSize: 6,
+    category: 'general',
+}
+
+News.propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+}
